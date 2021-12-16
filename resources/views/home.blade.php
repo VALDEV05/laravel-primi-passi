10b2ad0f505819888ac6a4d0f4b8732105d3d8b6
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-16 17:06:26 +0100

    Add style and write 'hello world'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
-</head>
+    <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <style>
+        body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+        }
+        .content {
+                text-align: center;
+            }
+
+        .title {
+                font-size: 84px;
+            }
+    </style>
+    </head>
 <body>
-    <h1>home</h1>
+    <div class="content">
+
+        <h1 class="title">Hello world</h1>
+    </div>
 </body>
 </html>
